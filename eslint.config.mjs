b74afb76e387d1d9719774ac240f6fe99f0c5cf0
Mostafa Ahmed Abdelglel <139139781+galelo04@users.noreply.github.com>
2026-08-26 15:40:00 +0300

    chore: scafolding nest project (#1)
    * chore: initialize nest project
    
    Signed-off-by: Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
    
    * feat: add Dockerfile for builds and docker compose for dev
    
    * chore: setup prisma
    
    Signed-off-by: Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
    
    * fix: postgresql volume path
    
    Signed-off-by: Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
    
    * fix: prisma config dotenv loading
    
    Signed-off-by: Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
    
    * chore: add a ci pipeline to build/lint:check/format:check/test
    
    Signed-off-by: Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
    
    * fix: linter errors in starter files
    
    Signed-off-by: Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
    
    ---------
    
    Signed-off-by: Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+      'no-duplicate-imports': ['error', { allowSeparateTypeImports: true }],
+    },
+  },
+  // Add this new block to target your e2e and unit tests
+  {
+    files: ['test/**/*.ts', '**/*.spec.ts'],
+    rules: {
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+    },
+  },
+);
