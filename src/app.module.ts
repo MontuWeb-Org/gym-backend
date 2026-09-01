d330d76529d731aae28c8692af6735a692aee8a2
Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
2026-09-01 16:15:45 +0300

    feat: add config module with zod schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaModule } from '@/prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 
+
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), RedisModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
